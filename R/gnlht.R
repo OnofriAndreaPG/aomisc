c9ef46cf8e54570e2b7316964543cbac9ee30c63
Andrea <onofriandreapg@gmail.com>
2020-01-09 17:59:30 +0100

    Update gnlht.R

--- R/gnlht.R
+++ R/gnlht.R
@@ -1,6 +1,8 @@
 gnlht <- function(obj, func,  const = NULL, vcov. = vcov, parameterNames=names(coef(obj))){
    # obj <- modNlin
-   # func <- c(~k1 - k2, ~k1 - k3, ~k1 - k4)
+   # func <- list(~ -log(prop)/k1, ~ -log(prop)/k2, ~ -log(prop)/k3, ~ -log(prop)/k4)
+   # const <- data.frame(prop = c(0.5, 0.7))
+   # 
    # Da qui
    temp <- lapply(func, function(x) as.character(as.expression(x[[length(x)]])))
    func <- data.frame(form=unlist(temp))
@@ -23,17 +25,19 @@ gnlht <- function(obj, func,  const = NULL, vcov. = vcov, parameterNames=names(c
      lisRes <- apply(const, 1, function(y) apply(func, 1, 
                    function(x) car::deltaMethod(object=coefs, g=x, 
                         vcov.=vcMat, level = 0.95, constants = y )))
-     val <- ldply(do.call(rbind, lisRes))
+     val <- plyr::ldply(do.call(rbind, lisRes))
      funList <- func %>% slice(rep(1:n(), each = length(const[,1])))
      constList <- const %>% slice(rep(1:n(), length(func[,1])))
      val <- cbind(funList, constList, val)
    }
-  retDF <- val[,1:3]
+  #val
+  lenVal <- length(val[1,]) 
+  retDF <- val[, c(-(lenVal-1), -lenVal)]
   retDF$"t-value" <- retDF$Estimate/retDF$SE
   if(class(obj) == "nls") resDF <- summary(obj)$df[2]
   else if(class(obj) == "drc") resDF <- summary(obj)$df
   else resDF <- Inf
-  print(resDF)
+  # print(resDF)
   retDF$"p-value" <- 2 * pt(retDF$"t-value", resDF, lower.tail = F)
   return(retDF)
 }
