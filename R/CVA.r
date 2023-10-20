84415f8b349590b83693f40d839af2bac1fd77ec
Andrea <onofriandreapg@gmail.com>
2023-10-20 15:02:43 +0200

    Updated CVA.r

--- R/CVA.r
+++ R/CVA.r
@@ -1,4 +1,4 @@
-CVA <- function(dataset, groups){
+CVA <- function(dataset, groups, scale = TRUE){
 ## Performs canonical variate analysis (date: 15/11/19)
 ## dataset is a multidimensional data.frame of observations
 ## groups is a vector coding for groupings
@@ -12,43 +12,44 @@ CVA <- function(dataset, groups){
   	canvarnames<-c(1:numcanonical)
 	  canvarnames<-paste("CAN", canvarnames,sep="")
   
-  	#Centratura e calcolo coefficienti canonici raw
-  	dataset.centrata <- scale(dataset, center = TRUE, scale = F)
-  	Tc <- t(dataset.centrata) %*% dataset.centrata
-  	i <- array(c(1:numcolonne,1:numcolonne),dim=c(numcolonne,2))
-  	dev <- Tc[i]
-  	devst <- sqrt(dev/(numdati-1))
-  	dataset.devst <- matrix(c(devst),numdati,numcolonne,byrow=TRUE)
+  	# Centratura e calcolo coefficienti canonici raw
+  	# dataset.centrata <- scale(dataset, center = TRUE, scale = F)
+  	# Tc <- t(dataset.centrata) %*% dataset.centrata
+  	# i <- array(c(1:numcolonne,1:numcolonne),dim=c(numcolonne,2))
+  	# dev <- Tc[i]
+  	# devst <- sqrt(dev/(numdati-1))
+  	# dataset.devst <- matrix(c(devst),numdati,numcolonne,byrow=TRUE)
+  	# 
+  	# maovst <- manova(dataset.centrata ~ groups)
+  	# fitted <- maovst$fitted
+  	# residui <- maovst$residuals
+  	# Bc <- t(fitted)%*%fitted
+  	# Wc <- t(residui)%*%residui
+  	# WBc <- solve(Wc) %*% Bc
+  	# V1c <- as.numeric(eigen(WBc)$vectors[,1:numcanonical])
+  	# V1c <- matrix(V1c, numcolonne, numcanonical)
+  	# scal <- sqrt( diag( t(V1c) %*% (Wc/(numdati - numclasses)) %*% V1c ) )
+  	# raw <- t( apply(V1c, 1, function(x) x / scal) )
+  	# if(nrow(raw) < 2) raw <- t(raw)
+  	# dimnames(raw) <- list(dimnames(dataset)[[2]], canvarnames)
+  	# # raw
   	
-  	maovst <- manova(dataset.centrata ~ groups)
-  	fitted <- maovst$fitted
-  	residui <- maovst$residuals
-  	Bc <- t(fitted)%*%fitted
-  	Wc <- t(residui)%*%residui
-  	WBc <- solve(Wc) %*% Bc
-  	V1c <- as.numeric(eigen(WBc)$vectors[,1:numcanonical])
-  	V1c <- matrix(V1c, numcolonne, numcanonical)
-  	scal <- sqrt( diag( t(V1c) %*% (Wc/(numdati - numclasses)) %*% V1c ) )
-  	raw <- t( apply(V1c, 1, function(x) x / scal) )
-  	if(nrow(raw) < 2) raw <- t(raw)
-  	dimnames(raw) <- list(dimnames(dataset)[[2]], canvarnames)
-  	# raw
+  	# Eventuale tandardizzazione e calcolo dei coefficienti canonici
+  	dataset.input <- scale(dataset, center = TRUE, scale = scale)
   	
-  	#Standardizzazione e calcolo coefficienti standardizzati
-  	dataset.standardizzata <- scale(dataset, center = TRUE, scale = TRUE)
-    maovst <- manova(dataset.standardizzata ~ groups)
+	  maovst <- manova(dataset.input ~ groups)
   	fitted <- maovst$fitted
   	residui<-maovst$residuals
-  	Ts <- t(dataset.standardizzata)%*%dataset.standardizzata
+  	Ts <- t(dataset.input)%*%dataset.input
   	B <- t(fitted)%*%fitted
   	W <- t(residui)%*%residui
-  	WB <- solve(W)%*%B
+  	WB <- solve(W) %*% B
   	
   	## This part calculates eigenvalues and eigenvectors
   	A <- as.numeric(eigen(WB)$values[1:numcanonical])
   	V1 <- as.numeric(eigen(WB)$vectors[,1:numcanonical])
   	V1 <- matrix(V1, numcolonne, numcanonical)
-	  VARCAN1 <- dataset.standardizzata %*% V1
+	  VARCAN1 <- dataset.input %*% V1
 	  if(length(VARCAN1[1,]) > 1) {
 	       maovst2 <- manova(VARCAN1 ~ groups)
 	  } else {
@@ -68,70 +69,74 @@ CVA <- function(dataset, groups){
   	# coefst
 
 ## calculation of raw canonical coefficients
-	  dev2 <- t(dataset.centrata)%*%dataset.centrata
-	  i <- array(c(1:numcolonne,1:numcolonne),dim=c(numcolonne,2))
-	  dev2 <- dev2[i]
-	  devst2 <- sqrt(dev2/(numdati))
-	  devst2 <- 1/devst2
-	  identita <- matrix(c(0),numcolonne,numcolonne)
-	  identita[i]<-devst2
-	  raw <- identita%*%coefst
-	  dimnames(raw) <- list(dimnames(dataset)[[2]],canvarnames)
+	  # dev2 <- t(dataset.centrata)%*%dataset.centrata
+	  # i <- array(c(1:numcolonne,1:numcolonne),dim=c(numcolonne,2))
+	  # dev2 <- dev2[i]
+	  # devst2 <- sqrt(dev2/(numdati))
+	  # devst2 <- 1/devst2
+	  # identita <- matrix(c(0),numcolonne,numcolonne)
+	  # identita[i]<-devst2
+	  # raw <- identita%*%coefst
+	  # dimnames(raw) <- list(dimnames(dataset)[[2]],canvarnames)
 
  ## canonical analysis
 	   prop <- A/sum(A)
-	   cancor<-sqrt(A/(1+A))
-	   cancor2<-cancor^2
+	   cancor <- sqrt(A/(1+A))
+	   cancor2 <- cancor^2
 
- ## scores of centroids (creati con gli standardised coefficients)
-	   medie <- aov(dataset.standardizzata~groups-1)$coefficients
+ ## scores of centroids
+	   medie <- aov(dataset.input ~ groups-1)$coefficients
      scores <- medie%*%coefst
-
+     rownames(scores) <- gsub("groups", "", rownames(scores))
      
  ## Classification functions
-     medie <- aov(as.matrix(dataset) ~ groups - 1)$coefficients
-     Vcov <- Wc/(numdati - numclasses)
-     constants <- diag(- 0.5 * medie %*% solve(Vcov) %*% t(medie) )
-     class.fun <- cbind(constants, t(solve(Vcov) %*% t(medie)) )
-  
-     matDati <- as.matrix( cbind(rep(1,numdati), dataset))
-     matCoef <- as.matrix( class.fun )
-     classVal <- matDati %*% t(matCoef)
-     class <- levels(groups)[apply(classVal, 1, function(x) which.max(x))]
+     # medie <- aov(as.matrix(dataset) ~ groups - 1)$coefficients
+     # Vcov <- W/(numdati - numclasses)
+     # constants <- diag(- 0.5 * medie %*% solve(Vcov) %*% t(medie) )
+     # class.fun <- cbind(constants, t(solve(Vcov) %*% t(medie)) )
+     # 
+     # matDati <- as.matrix( cbind(rep(1,numdati), dataset))
+     # matCoef <- as.matrix( class.fun )
+     # classVal <- matDati %*% t(matCoef)
+     # class <- levels(groups)[apply(classVal, 1, function(x) which.max(x))]
     
 
   ## canonical structure
-	   VARCAN <- dataset.standardizzata%*%coefst
+	   VARCAN <- dataset.input%*%coefst
 	   total <- cor(dataset, VARCAN)
 
-	   medie <- aov(dataset.centrata~groups-1)$fitted
-	   VARCANmedie <- medie %*% raw
+	   medie <- aov(dataset.input~groups-1)$fitted
+	   VARCANmedie <- medie %*% coefst
 	   between <- cor(medie,VARCANmedie)
 
-	   medie <- aov(as.matrix(dataset) ~ groups-1)$residuals
-	   VARCANresidui <- medie%*%raw
-	   within <- cor(medie,VARCANresidui)
+	   medie <- aov(as.matrix(dataset.input) ~ groups-1)$residuals
+	   VARCANresidui <- medie %*% coefst
+	   within <- cor(medie, VARCANresidui)
 
   ## RESULTS
-	list("Total deviances-codeviances" = Ts,
-	     "Between-groups deviances-codeviances" = B,
-	     "Within-groups deviances-codeviances" = W,
+	list(
+	     # MANOVA
+	     "TOT" = Ts,
+	     "B" = B,
+	     "W" = W,
 	     "B/W" = WB,
+	     # SPECTRAL DECOMPOSITION
 	     "eigenvalues" = A, 
-	     "eigenvectors.c" = V1c,
-	     "eigenvectors.s" = V1,
-	     "Proportion" = prop, 
-	     "Canonical.correlation" = cancor, 
+	     "eigenvectors" = V1,
+	     "proportion" = prop,
+	     # CANONICAL ANALYSES
+	     "correlation" = cancor, 
 	     "Squared.canonical.correlation" = cancor2, 
-	     "Standardised.coefficients"=coefst, 
-	     "Raw.coefficients"=raw,
-	     "Canonical.scores" = VARCAN,
-	     "Centroids.scores"=scores, 
+	     "coefficients"=coefst, 
+	     # "Raw.coefficients"=raw,
+	     "scores" = VARCAN,
+	     "centroids"=scores, 
 	     "Total.structure"=total, 
 	     "Between.structure"=between, 
-	     "Within.structure" = within,
-	     "Class.fun" = class.fun,
-	     "Class.val" = classVal,
-	     "Class" = class)
+	     "Within.structure" = within
+	     # "Class.fun" = class.fun,
+	     # "Class.val" = classVal,
+	     # "Class" = class
+	     )
 	
 }
